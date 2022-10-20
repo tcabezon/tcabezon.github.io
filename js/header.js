5df7863c1cf846d236a8b20cd73d9c3c0508732e
tcabezon <tcabezon@andrew.cmu.edu>
2022-10-20 10:24:57 -0400

    updating page

--- js/header.js
+++ js/header.js
@@ -16,23 +16,26 @@ document.write(`<!-- HEADER -->
                         </li>
                         <li><a href='projects.html'>Projects</a>
                             <ul class='sub-menu'>
-                            <li class='first' ><a href='hnx.html'>hnx.py</a></li>
-                            <li><a href='cyclegans.html'>CycleGANs for design</a></li>
-                            <li><a href='xyu.html'>XYU ring</a></li>
-                            <li><a href='IS.html'>Image synthesis</a></li>
-                            <li><a href='112.html'>15.112- 3D modelling</a></li>
-                            <li><a href='xyuWeb.html'>XYU web design</a></li>
-                            <li><a href='wearable.html'>Wearable design</a></li>
-                            <li><a href='back.html'>Back in the game Web</a></li>
-                            <li ><a href='TFG.html'>TFG</a></li>
-                            <li><a href='c2.html'>c2</a></li>
-                            <li><a href='miKitchen.html'>miKitchen</a></li>
-                            <li><a href='treeD.html'>treeD</a></li>
-                            <li><a href='synuous.html'>synuous</a></li>
-                            <li><a href='maskManiac.html'>MaskManiac</a></li>
-                            <li><a href='forsa.html'>Forså</a></li>
-                            <li><a href='nosotros.html'>Nosotros</a></li>
-                            <li><a href='courses.html'>Courses</a></li></ul>
+                                <!--<li class='first' ><a href='______.html'>______</a></li>--!>
+                                <li class='first' ><a href='genSys.html'>genSys</a></li>
+                                <li><a href='interClothesGAN.html'>InterClothesGAN</a></li>
+                                <li ><a href='hnx.html'>hnx.py</a></li>
+                                <li><a href='cyclegans.html'>CycleGANs for design</a></li>
+                                <li><a href='xyu.html'>XYU ring</a></li>
+                                <li><a href='IS.html'>Image synthesis</a></li>
+                                <li><a href='112.html'>15.112- 3D modelling</a></li>
+                                <li><a href='xyuWeb.html'>XYU web design</a></li>
+                                <li><a href='wearable.html'>Wearable design</a></li>
+                                <li><a href='back.html'>Back in the game Web</a></li>
+                                <li ><a href='TFG.html'>TFG</a></li>
+                                <li><a href='c2.html'>c2</a></li>
+                                <li><a href='miKitchen.html'>miKitchen</a></li>
+                                <li><a href='treeD.html'>treeD</a></li>
+                                <li><a href='synuous.html'>synuous</a></li>
+                                <li><a href='maskManiac.html'>MaskManiac</a></li>
+                                <li><a href='forsa.html'>Forså</a></li>
+                                <li><a href='nosotros.html'>Nosotros</a></li>
+                                <li><a href='courses.html'>Courses</a></li></ul>
                         </li>
                         <li>
                             <a href='contact.html'>Contact</a>
